d79f545da5ea1e2d97900a1afa33960cb29b5313
admin-24788 <benzina.elmahdi@gmail.com>
2023-10-17 16:21:14 +0200

    eureka server for registring service

---
+++ src/test/java/com/feignclient/eurikaserver/EurikaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feignclient.eurikaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurikaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
